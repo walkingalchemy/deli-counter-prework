2b3fb1ee0e76d9c8f0a856c972e4df71e8db1b60
James Royer <walkingalchemy@gmail.com>
2017-10-22 22:22:39 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def line(deli)
+  if deli == []
+    announce = "The line is currently empty."
+  else
+    announce = "The line is currently:"
+    deli.each {|person| announce.concat(" #{deli.find_index(person) + 1}. #{person}")}
+  end
+  puts announce
+end
+
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.find_index(name) + 1} in line."
+end
+
+def now_serving(deli)
+  if deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.shift}."
+  end
+end
