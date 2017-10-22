9617ed96972a219c45d391e72fe4298e0b56e4af
James Royer <walkingalchemy@gmail.com>
2017-10-22 22:02:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 def line(deli)
   if deli == []
-    puts "The line is currently empty."
+    announce = "The line is currently empty."
   else
     announce = "The line is currently:"
     deli.each {|person| annouce.append(" #{deli.find_index(person)}. #{person}")}
   end
+  announce
 end
