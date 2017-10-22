52777e706d21e4d646c038de7536cf7d32946522
James Royer <walkingalchemy@gmail.com>
2017-10-22 22:04:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     announce = "The line is currently empty."
   else
     announce = "The line is currently:"
-    deli.each {|person| announce.concat(" #{deli.find_index(person)}. #{person}")}
+    deli.each {|person| announce.concat(" #{deli.find_index(person) + 1}. #{person}")}
   end
   puts announce
 end
