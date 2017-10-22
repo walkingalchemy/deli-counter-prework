e8abb3af55d72bb7c81e75554dc84687e5c45bf2
James Royer <walkingalchemy@gmail.com>
2017-10-22 22:21:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,11 @@ def take_a_number(deli, name)
   deli.push(name)
   puts "Welcome, #{name}. You are number #{deli.find_index(name) + 1} in line."
 end
+
+def now_serving(deli)
+  if deli == []
+    puts "The line is currently empty."
+  else
+    puts "#{deli.pop} is served."
+  end
+end
